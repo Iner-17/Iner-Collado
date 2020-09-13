5d8a22038cb1d99474e4993641a903349b3989e8
Iner-17 <inercollado52@gmail.com>
2020-09-13 14:27:26 +0900

    fixed nav issue

--- main.js
+++ main.js
@@ -1,39 +1,12 @@
-//contact toggle
-let contactFormToggle = false;
-$("#contactForm").click(() => {
-  if (contactFormToggle == false) {
-    $(".container").css("filter", "blur(5px)");
-    $(".contact-form").css("display", "block");
-    $(".nav-links").css("width", "0px");
-    $(".nav-links li").css("display", "none");
-    $(".container").css("pointer-events", "none");
-    contactFormToggle = true;
-  }
-
-  if (contactFormToggle == true) {
-    $(".fa-times").click(() => {
-      $(".container").css("filter", "blur(0px)");
-      $(".contact-form").css("display", "none");
-      $(".nav-links").css("width", "0px");
-      $(".nav-links li").css("display", "none");
-      $(".container").css("pointer-events", "all");
-      contactFormToggle = false;
-    });
-  }
+$("#contactForm").click(function () {
+  $(".contact-form").toggleClass("show-contact-form");
+  $(".nav-links").toggleClass("show-nav-links");
 });
 
-//Toggle navbar
-let openClose = false;
-$(".menu-bar").click(() => {
-  if (openClose == false) {
-    //to open menu bar
-    $(".nav-links").css("width", "250px");
-    $(".nav-links li").css("display", "block");
-    openClose = true;
-  } else {
-    //to close menu bar
-    $(".nav-links").css("width", "0px");
-    $(".nav-links li").css("display", "none");
-    openClose = false;
-  }
+$(".fa-times").click(function () {
+  $(".contact-form").toggleClass("show-contact-form");
+});
+
+$(".menu-bar").click(function () {
+  $(".nav-links").toggleClass("show-nav-links");
 });
